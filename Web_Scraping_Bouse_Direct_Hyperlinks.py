caa0f32753a95750258c853577dc56f2b6629c93
Your Name <you@example.com>
2021-09-26 09:41:52 +0200

    Linting (Flake8)

--- Web_Scraping_Bouse_Direct_Hyperlinks.py
+++ Web_Scraping_Bouse_Direct_Hyperlinks.py
@@ -5,27 +5,31 @@ txt_path_Hyperlinks = "Web_Scraping_Bouse_Direct_Hyperlinks.txt"
 
 
 def update_txt_file(data):  # write data to a text file
-    with open(txt_path_Hyperlinks, "a",encoding='utf-8') as f:
+    with open(txt_path_Hyperlinks, "a", encoding='utf-8') as f:
         f.write(data + "\n")
 
 
 def clean_txt_file():
-    open(txt_path_Hyperlinks, "w",encoding='utf-8').close()
+    open(txt_path_Hyperlinks, "w", encoding='utf-8').close()
 
 
-def get_page_html():  # scrap the targeted website and return desired data in a tuple form
+def get_page_html():
+    # scrap the targeted website and return desired data in a tuple form
     url = "https://www.boursedirect.fr/fr/actualites/flux/aujourdhui"
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"
+        "User-Agent": """Mozilla/5.0 (Windows NT 10.0; Win64; x64)
+                         AppleWebKit/537.36 (KHTML, like Gecko)
+                         Chrome/83.0.4103.116 Safari/537.36"""
     }
     page = requests.get(url, headers=headers)
     soup = BeautifulSoup(page.content, "html.parser")
-    list_urls=[]
+    list_urls = []
 
     for title_news in soup.find_all("div", "timeline-body"):
         links = title_news.findAll("a", href=True)
         for el in links:
-             list_urls.append("https://www.boursedirect.fr" + str(el["href"])),  # hyperlink
+            # hyperlink
+            list_urls.append("https://www.boursedirect.fr" + str(el["href"])),
 
     for list_url in list_urls:
         update_txt_file(list_url)
@@ -33,5 +37,3 @@ def get_page_html():  # scrap the targeted website and return desired data in a 
 
 if __name__ == '__main__':
     get_page_html()
-
-
