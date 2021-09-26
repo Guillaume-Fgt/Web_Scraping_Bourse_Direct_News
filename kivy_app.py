6c68e87574e5626103c691a0c8ecd385c3362b6f
Your Name <you@example.com>
2021-09-26 09:17:57 +0200

    linting (Flake8)

--- kivy_app.py
+++ kivy_app.py
@@ -1,5 +1,3 @@
-from logging import root
-from kivy.app import App
 from kivy.config import Config
 Config.set('graphics', 'width', '530')
 Config.set('graphics', 'height', '700')
@@ -34,18 +32,27 @@ class NewsApp(MDApp):
 
 def refresh_news(self):
     try:
-        for i in range(1,21):
-            img=Image(source='img_articles/'+str(i)+'.jpg',allow_stretch= True, size_hint=(1,None), height='248dp', nocache=True)
+        for i in range(1, 21):
+            img = Image(source='img_articles/'+str(i)+'.jpg',
+                        allow_stretch=True, size_hint=(1, None),
+                        height='248dp', nocache=True)
             self.add_widget(img)
-            with open('Web_Scraping_Bouse_Direct.txt','r',encoding="utf-8")as file:
-                label_title=MDLabel(text=file.readlines()[i-1])
+            with open('Web_Scraping_Bouse_Direct.txt', 'r',
+                      encoding="utf-8")as file:
+                label_title = MDLabel(text=file.readlines()[i-1])
                 self.add_widget(label_title)
-            with open('Web_Scraping_Bouse_Direct_Hyperlinks.txt','r',encoding="utf-8")as file:
-                link_url=file.readlines()[i-1]
-                link_widget=MDChip(text='link', icon='link', text_color='white', icon_color='white',radius= [1,], on_press=lambda widget, link_url_inner=link_url:webbrowser.open(url=link_url_inner))
+            with open('Web_Scraping_Bouse_Direct_Hyperlinks.txt', 'r',
+                      encoding="utf-8")as file:
+                link_url = file.readlines()[i-1]
+                link_widget = MDChip(text='link', icon='link',
+                                     text_color='white', icon_color='white',
+                                     radius=[1, ], on_press=lambda widget,
+                                     link_url_inner=link_url:
+                                     webbrowser.open(url=link_url_inner))
                 self.add_widget(link_widget)
     except IndexError:
         self.clear_widgets()
-            
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     NewsApp().run()
